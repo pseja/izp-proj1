1b54096bf949686da98d597cb40d6e23a293675d
Lukas Pseja <luk4sps3j4@gmail.com>
2023-10-12 10:10:57 +0200

    Fixed bug where passing address itself as argument throws an error

--- src/keyfilter.c
+++ src/keyfilter.c
@@ -2,10 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
-#define MAX_LINE_LENGTH 101 // 101, because the length of line is 100 but +1 for '\0'
-#define MAX_UNIQUE_CHARS 69 // ascii chars from 32 to 126 without small letters
+#define MAX_LINE_LENGTH 103 // maximum length of line + '\0' + '\n' + '\r'
+#define MAX_UNIQUE_CHARS 69 // ascii characters from 32 to 126 without small letters
 
+int handleInput(int argc, char **input);
+int calculateInputIndex(char c);
+void handleOutput(int found, char *possibleString, char *possibleChars);
+
+int handleInput(int argc, char **input)
+{
+    // Checks if a file was passed as an argument to stdin
+    if (isatty(STDIN_FILENO))
+    {
+        printf("No file passed to stdin.\n");
+        return -1;
+    }
+
+    // No arguments passed except the program name
+    if (argc == 1)
+    {
+        *input = "";
+    }
+    else if (argc > 2)
+    {
+        printf("Too many arguments.\n");
+        return -2;
+    }
+
+    return 0; // OK
+}
+
+// Moving the ascii table to index 0 from whitespace(32) to `(96)
+// skipping small letters(97-122) because they will never occur, because
+// of toupper(c) getting passed into the function and then from {(123)
+// to ~(126)
 int calculateInputIndex(char c)
 {
     if (c >= 32 && c <= 96)
@@ -16,54 +48,12 @@ int calculateInputIndex(char c)
     {
         return c - 58;
     }
-    return EXIT_FAILURE;
+    fprintf(stderr, "Unexpected character\n");
+    return -1;
 }
 
-int main(int argc, char *argv[])
+void handleOutput(int found, char *possibleString, char *possibleChars)
 {
-    char *input = argv[1];
-    char address[MAX_LINE_LENGTH];
-    char possibleChars[MAX_UNIQUE_CHARS];
-
-    // Initializing array with '\0' to later input letters
-    for (int i = 0; i < MAX_UNIQUE_CHARS; i++)
-        possibleChars[i] = '\0';
-
-    char possibleString[MAX_LINE_LENGTH];
-    int found = 0;
-
-    // No argument passed edge case handling
-    if (argc < 2)
-        input = "";
-
-    // Loads lines to address variable until end of file is reached 
-    while (fgets(address, MAX_LINE_LENGTH, stdin) != NULL)
-    {
-        int i;
-        for (i = 0; i < (int)strlen(input); i++)
-        {
-            // If non matching character was found, break
-            if (toupper(input[i]) != toupper(address[i]))
-            {
-                break;
-            }
-        }
-
-        // Check if for loop matched the correct string with input
-        if (i == (int)strlen(input))
-        {
-            // Save the first thing that gets passed into possibleChars,
-            // for when that thing is the only one passed there i know
-            // what is the whole address
-            if (found == 0)
-                strcat(possibleString, address);
-
-            found++;
-            // Calculate where to put the current character
-            possibleChars[calculateInputIndex(toupper(address[i]))] = toupper(address[i]);
-        }
-    }
-
     if (found == 0)
     {
         printf("Not found\n");
@@ -72,7 +62,9 @@ int main(int argc, char *argv[])
     {
         printf("Found: ");
         for (int i = 0; i < MAX_LINE_LENGTH; i++)
+        {
             printf("%c", toupper(possibleString[i]));
+        }
     }
     else
     {
@@ -80,9 +72,81 @@ int main(int argc, char *argv[])
         for (int i = 0; i < MAX_UNIQUE_CHARS; i++)
         {
             if (possibleChars[i] != '\0')
+            {
                 printf("%c", possibleChars[i]);
+            }
         }
         printf("\n");
     }
+}
+
+int main(int argc, char *argv[])
+{
+    char *input = argv[1];
+
+    // Check if the input is correct
+    if (handleInput(argc, &input) != 0)
+    {
+        return 1;
+    }
+
+    char address[MAX_LINE_LENGTH];
+    char possibleChars[MAX_UNIQUE_CHARS];
+    char possibleString[MAX_LINE_LENGTH];
+    int found = 0;
+    int lengthOfInput = (int)strlen(input);
+
+    // Initializing array with '\0' to later input chars
+    for (int i = 0; i < MAX_UNIQUE_CHARS; i++)
+    {
+        possibleChars[i] = '\0';
+    }
+
+    // Loads lines to address variable until end of file is reached 
+    while (fgets(address, MAX_LINE_LENGTH, stdin) != NULL)
+    {
+        int i;
+
+        for (i = 0; i < lengthOfInput; i++)
+        {
+            // If non matching character was found, break
+            if (toupper(input[i]) != toupper(address[i]))
+            {
+                break;
+            }
+        }
+
+        // If the input is the same length + 1 (because of '\0' in address) 
+        // as the address and for loop passed all letters
+        // we have found the address we were looking for
+        if (strlen(address) == strlen(input)+1 && i == lengthOfInput)
+        {
+            strcat(possibleString, address);
+            found = 1;
+            break;
+        }
+        // Check if for loop matched the correct string with input
+        else if (i == lengthOfInput)
+        {
+            // Save the first address of the char that gets passed into possibleChars,
+            // for when that char is the only one passed there we know
+            // what is the whole address
+            if (found == 0)
+            {
+                strcat(possibleString, address);
+            }
+
+            found++;
+            // Calculate where to put the current character
+            int inputIndex = calculateInputIndex(toupper(address[i]));
+            if (inputIndex == -1)
+            {
+                return 2;
+            }
+            possibleChars[inputIndex] = toupper(address[i]);
+        }
+    }
+    handleOutput(found, possibleString, possibleChars);
+
     return 0;
 }
